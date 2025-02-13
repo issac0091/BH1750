245a6c740a5bca537960d562961a88964bc4aeb3
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-13 19:51:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 BH1750.on()
 BH1750.SetAddress(BH1750_ADDRESS.A35)
 OLED.init(128, 64)
+pins.analogWritePin(AnalogPin.P0, 0)
+pins.analogWritePin(AnalogPin.P1, 0)
+pins.analogWritePin(AnalogPin.P2, 0)
 basic.forever(function () {
     OLED.writeString("Lux:" + Math.floor(BH1750.getIntensity()))
     basic.pause(500)
     OLED.clear()
+})
+basic.forever(function () {
+    if (Math.floor(BH1750.getIntensity()) >= 530) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 0)
+    } else {
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.analogWritePin(AnalogPin.P1, 1023)
+        pins.analogWritePin(AnalogPin.P2, 0)
+    }
 })
